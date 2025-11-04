7b3bdf928f743d0ee0a70a96817bb5cdd8bc3529
DimasAbyanDiasta <dimas.abyan@ui.ac.id>
2025-11-05 06:25:37 +0700

    Initial commit - Tugas 7

---
+++ android/app/src/main/kotlin/com/example/bolabelishopmobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bolabelishopmobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'menu.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'bolabelishop',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.deepPurple,
+        ).copyWith(secondary: Colors.deepPurple[400]),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
